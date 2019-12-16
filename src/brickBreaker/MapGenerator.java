51c982e6fa85479d2d5d1404558f582782d2cd59
UKates <derrickcates@outlook.com>
2019-12-16 18:43:29 -0500

    Adding score and game reset logic

--- src/brickBreaker/MapGenerator.java
+++ src/brickBreaker/MapGenerator.java
@@ -22,7 +22,7 @@ public class MapGenerator {
         for(int i = 0; i < map.length; i++) {
             for(int j = 0; j < map[0].length; j++){
                 if(map[i][j] > 0) {
-                    g.setColor(Color.white);
+                    g.setColor(Color.blue);
                     g.fillRect(j * brickWidth + 80, i * brickHeight + 50,
                         brickWidth, brickHeight);
 
